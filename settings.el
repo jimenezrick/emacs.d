5bc44d4f5b8ebb697154932323d1f3b11c0d4a57
Ricardo Catalinas Jiménez <r@untroubled.be>
2019-06-22 20:23:26 +0100

    Enable ido mode

--- settings.el
+++ settings.el
@@ -8,6 +8,11 @@
 
 (global-set-key "\C-x\C-b" 'buffer-menu)
 
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(setq ido-use-filename-at-point 'guess)
+(ido-mode 1)
+
 (set-frame-font "Inconsolata-12")
 (menu-bar-mode -1)
 (tool-bar-mode -1)
