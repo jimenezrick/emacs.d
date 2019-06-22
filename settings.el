0b30504b9b231b16861a8daed2a52837eba5b4e7
Ricardo Catalinas Jiménez <r@untroubled.be>
2019-06-22 20:11:29 +0100

    Improve backups keeping multiple versions and copying

--- settings.el
+++ settings.el
@@ -1,4 +1,10 @@
-(setq backup-directory-alist '(("" . "~/.emacs.d/backup")))
+(setq
+  backup-by-copying t
+  backup-directory-alist '(("." . "~/.emacs.d/backup"))
+  delete-old-versions t
+  kept-new-versions 6
+  kept-old-versions 2
+  version-control t)
 
 (set-frame-font "Inconsolata-12")
 (menu-bar-mode -1)
