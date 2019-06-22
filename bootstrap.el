c67a563b1fe4bb6bfa4ba4ee3cd2a35b96202fb4
Ricardo Catalinas Jiménez <r@untroubled.be>
2019-06-22 22:10:41 +0100

    Bootstrap config installing use-package when missing

--- bootstrap.el
+++
@@ -1,6 +0,0 @@
-(load-file "repos.el")
-(package-initialize)
-(package-refresh-contents)
-
-(setq  package-selected-packages '(use-package))
-(package-install-selected-packages)
--- init.el
+++ init.el
@@ -6,6 +6,11 @@
 (load (my-get-fullpath "repos"))
 (package-initialize)
 
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (setq  package-selected-packages '(use-package))
+  (package-install-selected-packages))
+
 (eval-when-compile
   (require 'use-package))
 
