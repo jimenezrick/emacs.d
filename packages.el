fd37e5487fa4d823f023a8b2f290d594c9f98b58
Ricardo Catalinas Jiménez <r@untroubled.be>
2019-06-22 19:58:33 +0100

    Split config in multiple files

---
+++ packages.el
@@ -0,0 +1,40 @@
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1))
+
+(use-package spacemacs-theme
+  :ensure t
+  :no-require t
+  :config
+  (load-theme 'spacemacs-light t))
+
+(use-package speedbar
+  :ensure t)
+
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook)
+  (setq dashboard-center-content t)
+  (setq dashboard-startup-banner 'logo)
+  (setq dashboard-banner-logo-title nil)
+  (setq dashboard-set-init-info nil)
+  (setq dashboard-set-footer nil)
+  (setq dashboard-items '((recents . 10)
+                        (bookmarks . 5)
+                        (projects . 5)
+                        (registers . 5))))
+
+(use-package projectile
+  :ensure t
+  :config
+  (projectile-mode 1))
+
+(use-package recentf
+  :ensure t
+  :config
+  (recentf-mode 1))
+
+(use-package minimap
+  :ensure t)
---
+++ settings.el
@@ -0,0 +1,3 @@
+(set-frame-font "Inconsolata-12")
+(menu-bar-mode -1)
+(tool-bar-mode -1)
