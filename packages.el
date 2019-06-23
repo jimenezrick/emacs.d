eaf49c7e9dd0326c7f83275f8744fde2dba7ea46
Ricardo Catalinas Jiménez <r@untroubled.be>
2019-06-23 21:24:01 +0100

    Add ace-jump-mode package

--- packages.el
+++ packages.el
@@ -15,7 +15,8 @@
   (define-key evil-normal-state-map (kbd "C-k") (lambda () (interactive) (evil-previous-line 6)))
   (define-key evil-insert-state-map (kbd "C-x o") 'company-complete)
   (define-key evil-normal-state-map (kbd "C-p") 'helm-find-files)
-  (define-key evil-normal-state-map (kbd "SPC") 'helm-mini))
+  (define-key evil-normal-state-map (kbd "SPC") 'helm-mini)
+  (define-key evil-normal-state-map (kbd "s") 'ace-jump-mode))
 
 (use-package key-chord
   :ensure t
@@ -80,6 +81,9 @@
   :ensure t
   :config
   (company-mode))
+
+(use-package ace-jump-mode
+  :ensure t)
 
 (use-package rainbow-delimiters
   :ensure t
