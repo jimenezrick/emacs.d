5c8ba49e3f5b0ca917c59b3e88263f7e40610c5c
Ricardo Catalinas Jiménez <r@untroubled.be>
2019-06-23 21:11:12 +0100

    Add rainbow-delimiters package

--- packages.el
+++ packages.el
@@ -81,6 +81,11 @@
   :config
   (company-mode))
 
+(use-package rainbow-delimiters
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+
 ;; --- Color themes
 (use-package plan9-theme
   :ensure t)
