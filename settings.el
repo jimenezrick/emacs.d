5267efbb801017379ca0cd1f044a67bb7cf840c6
Ricardo Catalinas Jiménez <r@untroubled.be>
2019-06-23 12:05:11 +0100

    Disable ido, collides with helm

--- settings.el
+++ settings.el
@@ -11,12 +11,6 @@
 
 (global-display-line-numbers-mode)
 
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(setq ido-use-virtual-buffers t)
-(setq ido-use-filename-at-point 'guess)
-(ido-mode 1)
-
 (set-frame-font "Inconsolata-12")
 (menu-bar-mode -1)
 (tool-bar-mode -1)
