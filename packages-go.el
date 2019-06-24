79476d2e62866dfafe222826a186fd5c00db9580
Ricardo Catalinas Jiménez <r@untroubled.be>
2019-06-25 00:27:20 +0100

    Enable go company mode

--- packages-go.el
+++ packages-go.el
@@ -1,4 +1,10 @@
-(use-package go-eldoc)
+(use-package go-eldoc
+  :config
+  (setq company-tooltip-limit 20)
+  (setq company-idle-delay .3)
+  (setq company-echo-delay 0)
+  (setq company-begin-commands '(self-insert-command))
+  )
 
 (use-package company-go)
 
@@ -8,10 +14,11 @@
   :init
   (defun go-mode-setup ()
     (go-eldoc-setup)
-    (add-hook 'before-save-hook 'gofmt-before-save)
+    (go-projectile-tools-add-path)
     (setq gofmt-command "goimports")
     (add-hook 'before-save-hook 'gofmt-before-save)
-    (define-key evil-normal-state-map (kbd "C-]") 'godef-jump))
+    (define-key evil-normal-state-map (kbd "C-]") 'godef-jump)
+    (set (make-local-variable 'company-backends) '(company-go)) (company-mode))
   :config
   (go-mode)
   (add-hook 'go-mode-hook 'go-mode-setup))
