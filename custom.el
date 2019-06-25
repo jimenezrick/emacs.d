f979a6733dd628e5be61c41f7631341ba8781105
Ricardo Catalinas Jiménez <r@untroubled.be>
2019-06-25 13:29:16 +0100

    Update custom.el

--- custom.el
+++ custom.el
@@ -3,9 +3,23 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-begin-commands (quote (self-insert-command)))
+ '(company-echo-delay 0 t)
+ '(company-idle-delay 0.1)
+ '(company-minimum-prefix-length 2)
+ '(company-require-match nil)
+ '(company-selection-wrap-around t)
+ '(company-tooltip-align-annotations t)
+ '(company-tooltip-limit 20)
+ '(helm-M-x-fuzzy-match t t)
+ '(helm-buffers-fuzzy-matching t t)
+ '(helm-recentf-fuzzy-match t t)
  '(package-selected-packages
    (quote
-    (use-package spacemacs-theme projectile minimap evil dashboard))))
+    (use-package spacemacs-theme projectile minimap evil dashboard)))
+ '(treemacs-no-png-images t)
+ '(whitespace-display-mappings (quote ((space-mark 32 [183]) (tab-mark 9 [124 9]))))
+ '(whitespace-style (quote (face tabs tab-mark trailing))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
