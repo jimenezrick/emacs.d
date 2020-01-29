11027ef37f8f1d83321b6f1227498b85cf843d35
Ricardo Catalinas Jiménez <r@untroubled.be>
2020-01-29 02:51:50 +0000

    Choose theme based on the time of the day

--- packages-themes.el
+++ packages-themes.el
@@ -8,14 +8,16 @@
   :no-require t)
 
 (use-package spacemacs-theme
-  ; Hack needed for this package to work
-  :no-require t
+  :no-require t  ; Hack needed for this package to work
   :init
   (defun load-my-theme (&optional frame)
     (unless frame (setq frame (selected-frame)))
-    (if (display-graphic-p frame)
-        (load-theme 'spacemacs-light t)
-      (load-theme 'spacemacs-light t)))
+    (let ((hour (string-to-number (format-time-string "%H" (current-time)))))
+      (if (display-graphic-p frame)
+          (if (or (>= hour 21) (< hour 7))
+              (load-theme 'spacemacs-dark t)
+            (load-theme 'spacemacs-light t))
+        (load-theme 'spacemacs-light t))))
   :config
   (if (daemonp)
       (add-hook 'after-make-frame-functions
