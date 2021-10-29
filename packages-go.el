bd59e580b8a675e5d845e45d59ac8dab07d0a339
Ricardo Catalinas Jiménez <r@untroubled.be>
2021-10-29 12:31:12 +0100

    Use eglot for Go auto-completion
    Also to show documentation in the eldoc buffer.

--- packages-go.el
+++ packages-go.el
@@ -1,9 +1,3 @@
-(use-package go-eldoc)
-
-(use-package company-go
-  :custom
-  (company-go-show-annotation t))
-
 (use-package go-projectile)
 
 (use-package go-mode
@@ -11,11 +5,9 @@
   (gofmt-command "goimports")
   :init
   (defun go-mode-setup ()
-    (go-eldoc-setup)
+    (eglot-ensure)
     (go-projectile-tools-add-path)
     (add-hook 'before-save-hook 'gofmt-before-save)
-    (define-key evil-normal-state-map (kbd "C-]") 'godef-jump)
-    (define-key evil-normal-state-map (kbd "C-;") 'godoc-at-point)
-    (set (make-local-variable 'company-backends) '(company-go)) (company-mode))
+    (define-key evil-normal-state-map (kbd "C-]") 'godef-jump))
   :config
   (add-hook 'go-mode-hook 'go-mode-setup))
