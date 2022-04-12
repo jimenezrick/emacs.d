feeaba006bc33972febde2befdbc4959672ae56a
Ricardo Catalinas Jiménez <r@untroubled.be>
2022-04-12 17:56:26 +0100

    rust: Remove racer and use only eglot

--- packages-rust.el
+++ packages-rust.el
@@ -1,21 +1,9 @@
 (use-package rust-mode
   :custom
-  (rust-format-on-save t))
+  (rust-format-on-save t)
+  :config
+  (add-hook 'rust-mode-hook 'eglot-ensure))
 
 (use-package flycheck-rust
   :config
   (add-hook 'rust-mode-hook #'flycheck-rust-setup))
-
-(use-package racer
-  :custom
-  ;; FIXME: https://github.com/racer-rust/emacs-racer/issues/85
-  (racer-rust-src-path (concat (string-trim (shell-command-to-string "rustc --print sysroot"))
-                               "/lib/rustlib/src/rust/library"))
-  :config
-  (defun rust-mode-setup ()
-    (racer-mode)
-    (define-key evil-normal-state-map (kbd "C-]") 'racer-find-definition)
-    (define-key evil-normal-state-map (kbd "C-;") 'racer-describe-tooltip))
-  (add-hook 'rust-mode-hook #'rust-mode-setup)
-  (add-hook 'racer-mode-hook #'eldoc-mode)
-  (add-hook 'racer-mode-hook #'company-mode))
