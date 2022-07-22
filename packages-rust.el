659e6d93aebf171a8382e3eadc94080706e8bb2c
Ricardo Catalinas Jiménez <r@untroubled.be>
2022-07-22 14:15:34 +0100

    rust: Eanble rust-clippy checker

--- packages-rust.el
+++ packages-rust.el
@@ -6,4 +6,11 @@
 
 (use-package flycheck-rust
   :config
+  ;; TODO: Temporary fix (https://github.com/flycheck/flycheck/issues/1916)
+  (defun flycheck-rust-cargo-has-command-p (command)
+    (let ((cargo (funcall flycheck-executable-find "cargo")))
+      (cl-some (lambda (x) (string-prefix-p command x))
+               (mapcar #'string-trim-left
+                       (ignore-errors (process-lines cargo "--list"))))))
+  (flycheck-add-next-checker 'rust-cargo 'rust-clippy)
   (add-hook 'rust-mode-hook #'flycheck-rust-setup))
