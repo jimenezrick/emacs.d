51693f45b72d358d2fa5b9e3d29c39b1ce82303d
Ricardo Catalinas Jiménez <r@untroubled.be>
2024-03-11 16:20:13 +0000

    install-lsp-booster: fix path

--- scripts/install-lsp-booster.sh
+++ scripts/install-lsp-booster.sh
@@ -4,5 +4,5 @@ set -eu
 
 RELEASE=v0.2.0/emacs-lsp-booster_v0.2.0_x86_64-unknown-linux-musl.zip
 
-curl -L https://github.com/blahgeek/emacs-lsp-booster/releases/download/$RELEASE | funzip >~/emacs.d/emacs-lsp-booster
-chmod +x ~/emacs.d/emacs-lsp-booster
+curl -L https://github.com/blahgeek/emacs-lsp-booster/releases/download/$RELEASE | funzip >~/.emacs.d/emacs-lsp-booster
+chmod +x ~/.emacs.d/emacs-lsp-booster
