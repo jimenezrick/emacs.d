5956b5a169bf3ea1540849994752555af265574d
Ricardo Catalinas Jiménez <r@untroubled.be>
2024-06-12 10:31:36 +0100

    python: Enable ruff-isort reformatter

--- packages-python.el
+++ packages-python.el
@@ -1,3 +1,10 @@
 (use-package ruff-format
   :after python
-  :hook (python-ts-mode . ruff-format-on-save-mode))
+  :config
+  (reformatter-define ruff-isort
+    :program ruff-format-command
+    :args (list "check" "--select" "I" "--fix" "--stdin-filename" (or (buffer-file-name) input-file))
+    :lighter " RuffIsort"
+    :group 'ruff-format)
+  :hook (python-ts-mode . ruff-format-on-save-mode)
+  :hook (python-ts-mode . ruff-isort-on-save-mode))
