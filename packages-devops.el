628ac8fc950002c77c724fa0f011053ab2bdf67a
Ricardo Catalinas Jiménez <r@untroubled.be>
2025-01-10 11:25:31 +0000

    devops: Update jsonnet-mode for work

--- packages-devops.el
+++ packages-devops.el
@@ -2,7 +2,16 @@
 
 (use-package terraform-doc)
 
-(use-package jsonnet-mode)
+(use-package jsonnet-mode
+  :custom
+  (jsonnet-indent-level 4)
+  :config
+  (reformatter-define jsonnet-format
+    :program jsonnet-fmt-command
+    :args (list "-n" (number-to-string jsonnet-indent-level) "--string-style" "d" "--no-pad-arrays" "-")
+    :lighter " JnFmt"
+    :group 'jsonnet-format)
+  :hook (jsonnet-mode . jsonnet-format-on-save-mode))
 
 (use-package kubernetes
   :commands (kubernetes-overview)
