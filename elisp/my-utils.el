91f0ba919b56620e951a2e2033ef600e2266ecc4
Ricardo Catalinas Jiménez <r@untroubled.be>
2025-05-08 12:42:04 +0100

    my-utils: Add voice-transcribe interactive command

--- elisp/my-utils.el
+++ elisp/my-utils.el
@@ -2,4 +2,12 @@
   "Return the full path of *file-relative-path, relative to caller's file location."
   (concat (file-name-directory (or load-file-name buffer-file-name)) @file-relative-path))
 
+(defun voice-transcribe ()
+  "Transcribe the recorded speech with whisper.cpp inserting it into the current buffer."
+  (interactive)
+  (let ((script-path "/mnt/scratch-nvme/ricardo/WHISPER/whisper.cpp/whisper-transcribe-to-input.sh"))
+    (if (file-executable-p script-path)
+        (insert (shell-command-to-string (concat script-path " print")))
+      (message "Error: could not find voice-transcribe script: %s" script-path))))
+
 (provide 'my-utils)
