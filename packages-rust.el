6e70ac73228ab77da9037659c5c6269f491c33da
Ricardo Catalinas Jiménez <r@untroubled.be>
2026-03-23 11:22:05 +0000

    rust: Use derived mode with tree-sitter mode

--- packages-rust.el
+++ packages-rust.el
@@ -1,8 +1,11 @@
 (use-package rust-mode
+  :after treesit-auto
   :custom
+  (rust-mode-treesitter-derive t)
   (rust-rustfmt-switches '("--edition" "2024"))
   (rust-format-on-save t)
   :config
+  (delete 'rust treesit-auto-langs) ; Don't autoload rust-ts-mode (use instead derived rust-mode)
   (add-hook 'rust-mode-hook 'eglot-ensure))
 
 (use-package flycheck-rust
